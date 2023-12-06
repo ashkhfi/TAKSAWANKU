cfddbdf995b66ca983867c2a62ecb02bc8b1f295
askhkfi <ashkhfi.dev@gmail.com>
2023-12-06 22:19:18 +0700

    .

--- lib/pages/Materi.dart
+++ lib/pages/Materi.dart
@@ -30,40 +30,45 @@ class _MateriState extends State<Materi> {
         backgroundColor: Color(0xFFFFF6DC),
         centerTitle: true,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-                'assets/bg.jpg'), // Ganti dengan path gambar latar belakang Anda
-            fit: BoxFit.cover,
+      body: Column(
+        children: [
+          
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/bg.jpg'), // Ganti dengan path gambar latar belakang Anda
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                itemCount: daftarMateri.length,
+                itemBuilder: (context, index) {
+                  MateriModel materi = daftarMateri[index];
+                  return Card(
+                    child: ListTile(
+                      tileColor:Color(0xFFFAEED1),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      leading: const Icon(Icons.book, size: 30),
+                      title: Text(materi.nama),
+                      // Tambahkan aksi atau fungsi yang diinginkan saat ListTile diklik
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ViewPdf(title: materi.nama, file: materi.file)),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            itemCount: daftarMateri.length,
-            itemBuilder: (context, index) {
-              MateriModel materi = daftarMateri[index];
-              return Card(
-                child: ListTile(
-                  tileColor:Color(0xFFFAEED1),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  leading: const Icon(Icons.book, size: 30),
-                  title: Text(materi.nama),
-                  // Tambahkan aksi atau fungsi yang diinginkan saat ListTile diklik
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              ViewPdf(title: materi.nama, file: materi.file)),
-                    );
-                  },
-                ),
-              );
-            },
-          ),
-        ),
+        ],
       ),
     );
   }
